22852b7b887e33d0fd96657cde2592f46dec089d
Manisangh Sharma <manisanghsharma@gmail.com>
2024-10-27 22:54:22 +0530

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Pressable } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+      <TouchableOpacity onPress={() => console.log("Pressed")} activeOpacity={0.8}>
+        <Text>
+          Delete
+        </Text>
+        </TouchableOpacity>
+			<View style={styles.itemContainer}>
+				<Text style={{fontSize: 18, fontWeight: "300"}}>Coffee</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		justifyContent: "center",
+	},
+	itemContainer: {
+		paddingVertical: 16,
+		paddingHorizontal: 8,
+		borderBottomWidth: 1,
+		borderBottomColor: "cadetblue",
+	},
 });
